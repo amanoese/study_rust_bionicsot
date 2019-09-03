865e364fba301d47a2b968e57d1989d4b1ce813e
amanoese <amanoese@gmail.com>
2019-09-04 03:06:42 +0900

    add test case

--- src/utils.rs
+++ src/utils.rs
@@ -6,13 +6,38 @@ pub fn new_u32_vec(n: usize) -> Vec<u32> {
     // init RNG
     let mut rng = Pcg64Mcg::from_seed([0; 16]);
 
-    // init Vec
-    let mut v = Vec::with_capacity(n);
-
-    for _ in 0..n {
-        v.push(rng.sample(&Standard));
-    };
-
-    v
+    rng.sample_iter(&Standard).take(n).collect()
 }
 
+// listが昇順かの確認
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+
+    // windows(2) はリストを１要素刻みで２要素づつ返す
+    // [ 1, 2 ,3 ,4 ] なら [1, 2] [2, 3] [3, 4] を返す
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_decending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
+}
+
+mod tests {
+    use crate::utils::{new_u32_vec,is_sorted_ascending,is_sorted_decending};
+    use crate::third::sort;
+    use crate::SortOrder::*;
+
+    #[test]
+    fn sort_32_large() {
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x,&Ascending),Ok(()));
+            assert!(is_sorted_ascending(&x))
+        }
+        {
+            let mut x = new_u32_vec(65536);
+            assert_eq!(sort(&mut x,&Decending),Ok(()));
+            assert!(is_sorted_decending(&x))
+        }
+
+    }
+}
